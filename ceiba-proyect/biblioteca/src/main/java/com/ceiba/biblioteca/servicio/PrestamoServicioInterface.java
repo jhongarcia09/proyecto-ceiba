1c40eacc515b9f8b9dcf9381df260a28fcf442b4
JoseaSosa <josea_1998@htomail.com>
2023-06-01 12:44:25 -0500

    se crea busqueda por id

--- ceiba-proyect/biblioteca/src/main/java/com/ceiba/biblioteca/servicio/PrestamoServicioInterface.java
+++ ceiba-proyect/biblioteca/src/main/java/com/ceiba/biblioteca/servicio/PrestamoServicioInterface.java
@@ -7,4 +7,5 @@ import java.time.LocalDate;
 public interface PrestamoServicioInterface {
     Prestamo crearPrestamo(Prestamo prestamo) throws Exception;
     LocalDate calcularfechaMaximadevolucion(Prestamo prestamo);
+    Prestamo buscarPorid(Integer id) throws Exception;
 }
